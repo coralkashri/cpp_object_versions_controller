e2d65abcffcb2f24f1685dd7bd1d2cc5a8ce5909
Korel Kashri <korelkashri@gmail.com>
2020-09-04 05:15:28 +0300

    Create a simple object & collection versions history.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "versions_controller.h"
 
-struct patch : patch_interface {
+struct my_simple_patch : patch_interface {
     class new_element {
     public:
         explicit new_element(int val) : val(val) {}
@@ -22,12 +22,12 @@ struct patch : patch_interface {
         int val;
     };
 
-    explicit patch(new_element patch_new_element) : val(patch_new_element.get()), relative_element_idx(-1) {};
-    explicit patch(update_element patch_update_element) : val(patch_update_element.get_val()), relative_element_idx(patch_update_element.get_relative_element_idx()) {};
-    patch(const patch&) = default;
-    patch(patch&&) = default;
-    patch& operator=(const patch&) = default;
-    patch& operator=(patch&&) = default;
+    explicit my_simple_patch(new_element patch_new_element) : val(patch_new_element.get()), relative_element_idx(-1) {};
+    explicit my_simple_patch(update_element patch_update_element) : val(patch_update_element.get_val()), relative_element_idx(patch_update_element.get_relative_element_idx()) {};
+    my_simple_patch(const my_simple_patch&) = default;
+    my_simple_patch(my_simple_patch&&) = default;
+    my_simple_patch& operator=(const my_simple_patch&) = default;
+    my_simple_patch& operator=(my_simple_patch&&) = default;
 
     [[nodiscard]] int get_relative_element_idx() const { return relative_element_idx; }
     [[nodiscard]] int get_val() const { return val; }
@@ -37,7 +37,93 @@ private:
     int val;
 };
 
+class my_simple_object {
+public:
+    explicit my_simple_object(int val) : val(val) {}
+
+    void set_val(int new_val) { val = new_val; }
+    [[nodiscard]] int get_val() const { return val; }
+
+private:
+    int val;
+};
+
+class my_simple_collection {
+public:
+    void add_new_element(int val) {
+        vec.emplace_back(val);
+        history.create_new_patch(my_simple_patch(my_simple_patch::new_element(val)));
+    }
+
+    void update_element(size_t index, int val) {
+        vec.at(index).set_val(val);
+        history.create_new_patch(my_simple_patch(my_simple_patch::update_element(index, val)));
+    }
+
+    void close_version() {
+        history.close_version();
+    }
+
+    void apply_version(const version<my_simple_patch> &ver) {
+        auto patches = ver.clone_new_patches(history.get_current_patch_number());
+        for (auto &patch : patches) {
+            if (auto index = patch.get_relative_element_idx(); index == -1) {
+                add_new_element(patch.get_val());
+            } else {
+                update_element(index, patch.get_val());
+            }
+        }
+    }
+
+    void apply_and_close_version(const version<my_simple_patch> &ver) {
+        apply_version(ver);
+        close_version();
+    }
+
+    version<my_simple_patch> get_current_version() {
+        return history.get_last_version();
+    }
+
+    version<my_simple_patch> get_and_close_current_version() {
+        auto current_version = history.get_last_version();
+        history.close_version();
+        return current_version;
+    }
+
+    void print() {
+        for (auto &elem : vec) {
+            std::cout << elem.get_val() << " ";
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    std::vector<my_simple_object> vec;
+    versions_controller<my_simple_patch> history;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    my_simple_collection src, copy;
+    src.add_new_element(5);
+    src.add_new_element(6);
+    src.add_new_element(7);
+    copy.apply_and_close_version(src.get_and_close_current_version());
+    src.print(); copy.print();
+    src.add_new_element(8);
+    src.add_new_element(9);
+    src.add_new_element(10);
+    src.add_new_element(11);
+    src.add_new_element(12);
+    src.add_new_element(13);
+    copy.apply_and_close_version(src.get_and_close_current_version());
+    src.print(); copy.print();
+    src.update_element(3, 80);
+    src.add_new_element(14);
+    src.add_new_element(15);
+    copy.apply_and_close_version(src.get_and_close_current_version());
+    src.print(); copy.print();
+    copy.apply_and_close_version(src.get_and_close_current_version()); // Nothing happens here
+    copy.apply_and_close_version(src.get_and_close_current_version()); // Nothing happens here
+    src.print(); copy.print();
+    return EXIT_SUCCESS;
 }
--- versions_controller.h
+++ versions_controller.h
@@ -17,28 +17,108 @@ concept PatchInterface = std::is_base_of_v<patch_interface, T>;
 template <PatchInterface PI>
 struct version {
 public:
+    explicit version(size_t patch_number) : patch_number(patch_number) {};
+
+    version(const version<PI> &ref) : patch_number(ref.patch_number), patches(ref.clone_all_patches()) {};
+
+    version(version<PI> &&ref) noexcept : patch_number(ref.patch_number), patches(ref.clone_all_patches()) {};
+
+    /**
+     * Create a new patch to this version.
+     * @param patch - The desired patch.
+     */
     void create_new_patch(PI patch) {
+        std::lock_guard g(guarder);
         patches.emplace_back(patch);
+        patch_number++;
     };
 
-    std::vector<PI> clone_patches() const {
+    /**
+     * @param last_patch_number - Desired patch number to start clone from.
+     *
+     * @return A copy vector of this version's patches, started from { last_patch_number - (patch_number - patches.size()) }.
+     */
+    std::vector<PI> clone_new_patches(size_t last_patch_number) const {
+        std::lock_guard g(guarder);
+        return std::vector(patches.begin() + last_patch_number - (patch_number - patches.size()), patches.end());
+    }
+
+    /**
+     * @return All of this version's patches.
+     */
+    std::vector<PI> clone_all_patches() const {
+        std::lock_guard g(guarder);
         return patches;
     }
 
 private:
+    mutable std::mutex guarder;
     std::vector<PI> patches;
+    size_t patch_number;
 };
 
 template <PatchInterface PI>
 struct versions_controller {
 public:
-    version<PI>& get_last_version() const { std::shared_lock g(guarder); return get_version(versions.size() - 1); }
-    version<PI>& get_version(std::size_t number) const { std::shared_lock g(guarder); return std::advance(versions.begin(), number); }
-    void create_new_version() { std::unique_lock g(guarder); versions.emplace_back(); }
+    explicit versions_controller(size_t max_versions_count = 0) : max_versions_count(max_versions_count), last_version_status(CLOSE), patch_number(0) {}
+
+    /**
+     * @return The last version (copy).
+     */
+    version<PI> get_last_version() { std::shared_lock g(guarder); return find_version(versions.size() - 1); }
+
+    /**
+     * @param number - Target version number.
+     * @return A target version copy.
+     */
+    version<PI> get_version(size_t number) { std::shared_lock g(guarder); return find_version(number); }
+
+    /**.
+     * If the current version is open - create a new patch in the current version.
+     * Otherwise create a new version with the desired patch.
+     * @param patch - The desired patch.
+     */
+    void create_new_patch(PI patch) {
+        std::unique_lock g(guarder);
+        if (last_version_status == CLOSE) {
+            versions.emplace_back(patch_number); // Create a new version
+            last_version_status = OPEN;
+        }
+        find_version(versions.size() - 1).create_new_patch(patch);
+        patch_number++;
+    }
+
+    /**
+     * Close the current version for new patches
+     */
+    void close_version() {
+        std::shared_lock g(guarder);
+        last_version_status = CLOSE;
+    }
+
+    /**
+     * Remove all versions from this controller.
+     */
+    void clear_versions() { std::unique_lock g(guarder); versions.clear(); }
+
+    size_t get_current_patch_number() { std::shared_lock g(guarder); return patch_number; }
 
 private:
     std::shared_mutex guarder;
     std::list<version<PI>> versions;
+    size_t max_versions_count;
+    size_t patch_number;
+
+    enum version_status : bool {
+        CLOSE = 0,
+        OPEN = 1
+    } last_version_status;
+
+    version<PI>& find_version(size_t number) {
+        auto ver_it = versions.begin();
+        std::advance(ver_it, number);
+        return *ver_it;
+    }
 };
 
 #endif //CPP_OBJECT_VERSION_CONTROLLER_VERSIONS_CONTROLLER_H
